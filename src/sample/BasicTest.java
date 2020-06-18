f0c47173e365eaf3ecdae3d05666e9c65c0dd757
girirajvengurlekar <girirajvengurlekar@users.noreply.github.com>
2020-06-18 12:25:45 +0530

    Commit

---
+++ src/sample/BasicTest.java
@@ -0,0 +1,32 @@
+package sample;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BasicTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//setting the driver executable
+		System.setProperty("webdriver.chrome.driver", "C:\\Software\\chromedriver_83\\chromedriver.exe");
+
+		//Initiating your chromedriver
+		WebDriver driver=new ChromeDriver();
+
+		//Applied wait time
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		//maximize window
+		driver.manage().window().maximize();
+
+		//open browser with desried URL
+		driver.get("https://www.google.com");
+
+		//closing the browser
+		driver.close();
+
+	}
+
+}
